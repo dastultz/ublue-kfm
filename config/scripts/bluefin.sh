0ee0b71aab24d05e4bb4c8f98962d649d266c292
Daryl Stultz <kungfumachinist@gmail.com>
2023-09-24 17:07:04 -0400

    move service start to recipe

--- config/scripts/bluefin.sh
+++ config/scripts/bluefin.sh
@@ -10,4 +10,3 @@ fc-cache -f /usr/share/fonts/inter
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
 sed -i '/^PRETTY_NAME/s/Silverblue/uBlue-KFM/' /usr/lib/os-release
-systemctl enable rpm-ostree-countme.service
