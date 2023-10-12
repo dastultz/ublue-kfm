8a7162f1af35609dcd9421030a5374d68a9eba1d
Daryl Stultz <kungfumachinist@gmail.com>
2023-10-12 19:48:23 -0400

    theme / desktop

--- config/scripts/install-whitesur-theme.sh
+++ config/scripts/install-whitesur-theme.sh
@@ -10,4 +10,4 @@ cd WhiteSur-gtk-theme
 sed -i 's|-w "/root"|true|g' shell/lib-flatpak.sh
 sed -i 's|-w "/root"|true|g' shell/lib-core.sh
 
-sudo ./tweaks.sh --gdm -c Dark -N -n -b /usr/share/backgrounds/login.jpg --silent-mode
+sudo ./tweaks.sh --gdm -c Dark -N -n -b /usr/share/backgrounds/fedora-workstation/futurecity_dark.webp --silent-mode -i fedora
