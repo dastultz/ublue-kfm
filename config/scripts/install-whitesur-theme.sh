82783a45775b355c5323c3d0f7bcdaafe24d354e
Daryl Stultz <kungfumachinist@gmail.com>
2023-11-09 21:36:26 -0500

    no icon for top bar

--- config/scripts/install-whitesur-theme.sh
+++ config/scripts/install-whitesur-theme.sh
@@ -10,4 +10,4 @@ cd WhiteSur-gtk-theme
 sed -i 's|-w "/root"|true|g' shell/lib-flatpak.sh
 sed -i 's|-w "/root"|true|g' shell/lib-core.sh
 
-sudo ./tweaks.sh --gdm -c Dark -N -n -b /usr/share/backgrounds/login.jpg --silent-mode -i fedora
+sudo ./tweaks.sh --gdm -c Dark -N -n -b /usr/share/backgrounds/login.jpg --silent-mode
